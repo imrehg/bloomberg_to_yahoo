af2d657375bc0324c63a28e06f3a5be0be05be91
Gergely Imreh <gergely@imreh.net>
2024-05-18 19:53:05 +0800

    code adjustments and cleanup

--- toyahoo.py
+++ toyahoo.py
@@ -6,6 +6,8 @@ from datetime import date
 
 @dataclass
 class Lot:
+    """Lot data in format roughly matching Yahoo Finance's"""
+
     symbol: str
     trade_date: date
     purchase_price: float
@@ -14,15 +16,19 @@ class Lot:
     comment: str = ""
 
 
-def field_names_capitalize(field: str) -> str:
+def field_names_conversion(field: str) -> str:
     """Capitalize the field names"""
+    # TODO: make this a mapping so the internal representation can be different
     return field.replace("_", " ").title()
 
 
 def lot_to_line(lot: Lot) -> dict:
     """Convert a Lot object to a dictionary"""
-    entry: dict = {field_names_capitalize(k): v for (k, v) in asdict(lot).items()}
+    entry: dict = {field_names_conversion(k): v for (k, v) in asdict(lot).items()}
+    # Yahoo Finance requires the date in the format YYYYMMDD
     entry["Trade Date"] = lot.trade_date.strftime("%Y%m%d")
+    # Yahoo Finance stores stock info differently from Bloomberg
+    # TODO: do more robust conversion
     entry["Symbol"] = entry["Symbol"].replace(":", ".").replace("TT", "TW")
     return entry
 
@@ -50,11 +56,6 @@ def export(portfolio, output):
             stockswriter.writerow(lot_to_line(lot))
 
 
-def date_conversion(timestamp: int) -> date:
-    """Convert the Bloomberg timestamp to an inner date object"""
-    return date.fromtimestamp(timestamp / 1000)
-
-
 def main():
     """Process the watchlist JSON file"""
     with open("watchlists.json", "r") as watchlist:
@@ -66,17 +67,15 @@ def main():
     holdings: list[Lot] = []
 
     for position in positions:
-        print(position["security"]["ticker"], len(position["lots"]))
         lots = position["lots"]
         for lot in lots:
             entry = Lot(
                 symbol=position["security"]["ticker"],
-                trade_date=date_conversion(lot["shares"]["buydate"]),
+                trade_date=date.fromtimestamp(lot["shares"]["buydate"] / 1000),
                 purchase_price=lot["shares"]["buyprice"],
                 quantity=lot["shares"]["number"],
                 comment="Import from Bloomberg",
             )
-            print(entry)
             holdings.append(entry)
 
     export(holdings, "upload.csv")
